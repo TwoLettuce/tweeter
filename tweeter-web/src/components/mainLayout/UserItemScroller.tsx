5c32f2ee14c66e547775c0045677ebc7c3efbc73
Grant Harris <grantharris0323@gmail.com>
2026-09-17 11:58:53 -0600

    M1 refactor Followees/Followers scrollers

--- tweeter-web/src/components/mainLayout/UserItemScroller.tsx
+++ tweeter-web/src/components/mainLayout/UserItemScroller.tsx
@@ -1,19 +1,28 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useParams } from "react-router-dom";
+import { User, AuthToken, FakeData } from "tweeter-shared";
+import { ToastType } from "../toaster/Toast";
+import { ToastActionsContext } from "../toaster/ToastContexts";
 import {
   UserInfoContext,
   UserInfoActionsContext,
 } from "../userInfo/UserInfoContexts";
-import { useState, useEffect } from "react";
-import InfiniteScroll from "react-infinite-scroll-component";
-import { AuthToken, FakeData, User } from "tweeter-shared";
-import { ToastActionsContext } from "../toaster/ToastContexts";
-import { useParams } from "react-router-dom";
-import { ToastType } from "../toaster/Toast";
 import UserItem from "../userItem/UserItem";
 
-export const PAGE_SIZE = 10;
+const PAGE_SIZE = 10;
 
-const FollowersScroller = () => {
+interface Props {
+  itemDescription: string;
+  featureUrl: string;
+  loadMore: (
+    authToken: AuthToken,
+    userAlias: string,
+    pageSize: number,
+    lastItem: User | null,
+  ) => Promise<[User[], boolean]>;
+}
+const UserItemScroller = (props: Props) => {
   const { displayToast } = useContext(ToastActionsContext);
   const [items, setItems] = useState<User[]>([]);
   const [hasMoreItems, setHasMoreItems] = useState(true);
@@ -25,14 +34,6 @@ const FollowersScroller = () => {
   const { displayedUser, authToken } = useContext(UserInfoContext);
   const { setDisplayedUser } = useContext(UserInfoActionsContext);
   const { displayedUser: displayedUserAliasParam } = useParams();
-
-  const getUser = async (
-    authToken: AuthToken,
-    alias: string
-  ): Promise<User | null> => {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.findUserByAlias(alias);
-  };
 
   // Update the displayed user context variable whenever the displayedUser url parameter changes. This allows browser forward and back buttons to work correctly.
   useEffect(() => {
@@ -63,11 +64,11 @@ const FollowersScroller = () => {
 
   const loadMoreItems = async (lastItem: User | null) => {
     try {
-      const [newItems, hasMore] = await loadMoreFollowers(
+      const [newItems, hasMore] = await props.loadMore(
         authToken!,
         displayedUser!.alias,
         PAGE_SIZE,
-        lastItem
+        lastItem,
       );
 
       setHasMoreItems(() => hasMore);
@@ -76,20 +77,28 @@ const FollowersScroller = () => {
     } catch (error) {
       displayToast(
         ToastType.Error,
-        `Failed to load followers because of exception: ${error}`,
-        0
+        `Failed to load ${props.itemDescription} because of exception: ${error}`,
+        0,
       );
     }
   };
 
-  const loadMoreFollowers = async (
+  const loadMoreFollowees = async (
     authToken: AuthToken,
     userAlias: string,
     pageSize: number,
-    lastFollower: User | null
+    lastFollowee: User | null,
   ): Promise<[User[], boolean]> => {
     // TODO: Replace with the result of calling server
-    return FakeData.instance.getPageOfUsers(lastFollower, pageSize, userAlias);
+    return FakeData.instance.getPageOfUsers(lastFollowee, pageSize, userAlias);
+  };
+
+  const getUser = async (
+    authToken: AuthToken,
+    alias: string,
+  ): Promise<User | null> => {
+    // TODO: Replace with the result of calling server
+    return FakeData.instance.findUserByAlias(alias);
   };
 
   return (
@@ -106,7 +115,7 @@ const FollowersScroller = () => {
             key={index}
             className="row mb-3 mx-0 px-0 border rounded bg-white"
           >
-            <UserItem user={item} featurePath="/followers" />
+            <UserItem user={item} featurePath={props.featureUrl} />
           </div>
         ))}
       </InfiniteScroll>
@@ -114,4 +123,4 @@ const FollowersScroller = () => {
   );
 };
 
-export default FollowersScroller;
+export default UserItemScroller;
