fd8613120cc967ca62108e499b7b8b3b88f5ae96
TwoLettuce <grantharris0323@gmail.com>
2026-09-22 20:25:16 -0600

    M1 create UserNavigationHook

---
+++ tweeter-web/src/components/appNavbar/UserNavigationHook.ts
@@ -0,0 +1,45 @@
+import { AuthToken, FakeData, User } from "tweeter-shared";
+import { useUserInfo } from "../userInfo/UserInfoHook";
+import { useUserInfoActions } from "../userInfo/UserInfoActionsHook";
+import { useMessageActions } from "../toaster/MessageHooks";
+import { useNavigate } from "react-router-dom";
+
+const { displayedUser, authToken } = useUserInfo();
+const { setDisplayedUser } = useUserInfoActions();
+const { displayErrorMessage } = useMessageActions();
+const navigate = useNavigate();
+
+const extractAlias = (value: string): string => {
+  const index = value.indexOf("@");
+  return value.substring(index);
+};
+
+const getUser = async (
+  authToken: AuthToken,
+  alias: string,
+): Promise<User | null> => {
+  // TODO: Replace with the result of calling server
+  return FakeData.instance.findUserByAlias(alias);
+};
+
+export const useUserNavigation = async (
+  event: React.MouseEvent,
+  featureUrl: string,
+): Promise<void> => {
+  event.preventDefault();
+
+  try {
+    const alias = extractAlias(event.target.toString());
+
+    const toUser = await getUser(authToken!, alias);
+
+    if (toUser) {
+      if (!toUser.equals(displayedUser!)) {
+        setDisplayedUser(toUser);
+        navigate(`${featureUrl}/${toUser.alias}`);
+      }
+    }
+  } catch (error) {
+    displayErrorMessage(`Failed to get user because of exception: ${error}`);
+  }
+};
